d1b75c9cdaa8f2ff2490080f8084733cb3afeae3
puurp756 <231892235+puurp756@users.noreply.github.com>
2025-09-16 12:26:52 +0200

    Corrections

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Denombrements
 {
     class Program
     {
+        /// <summary>
+        /// Calcule la factorielle d'un nombre.
+        /// </summary>
+        /// <param name="subsetSize">Le nombre dont on veut calculer la factorielle.</param>
+        /// <returns>Le résultat de la factorielle de subsetSize (subsetSize!).</returns>
+        static long CalculR(int subsetSize)
+        {
+            long factorialResult = 1;
+            for (int k = 1; k <= subsetSize; k++)
+                factorialResult *= k;
+            return factorialResult;
+
+
+        }
+
+        static long CalculR2(int subsetSize, int t)
+        {
+            long factorialResult = 1;
+            for (int k = (t - subsetSize + 1); k <= t; k++)
+                factorialResult *= k;
+            return factorialResult;
+
+        }
+
         static void Main(string[] args)
         {
+            //Boucle les saisies tant que la variable c est different de 0 et permer de lancer le jeu.
+
             int c = 1;
-            while (c != 0)
+            while (c != 0) 
             {
                 Console.WriteLine("Permutation ...................... 1");
                 Console.WriteLine("Arrangement ...................... 2");
@@ -20,54 +42,64 @@ namespace Denombrements
                 Console.Write("Choix :                            ");
                 c = int.Parse(Console.ReadLine());
 
-                if (c == 0) { Environment.Exit(0); }
 
-                if (c == 1)
+                //Differentes conditions afin de s'assurer que chaque saisie execute un bloc de code qui lui est propre.
+
+                //Recommencement de la boucle si saisie = 4 ou supérieur.
+                if (c >= 4)
                 {
-                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                    int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                           // calcul de r
-                    long r = 1;
-                    for (int k = 1; k <= n; k++)
-                        r *= k;
-                    Console.WriteLine(n + "! = " + r);
+                    Console.WriteLine("Mentionnez les bons nombres");
+                    continue;
                 }
-                else
+
+                // sortie du programme si la saisie correspond à 0                                     
+                else if (c == 0)
                 {
-                    if (c == 2)
-                    {
+                    Environment.Exit(0);
+                }
+
+                // Calcul la permutation via une fonction CalculR si saisie = 1
+                else if (c == 1)
+                {
+                    Console.Write("nombre total d'éléments à gérer = "); 
+                    int subsetSize = int.Parse(Console.ReadLine());                                   
+                    long factorialResult = CalculR(subsetSize);
+                    Console.WriteLine(subsetSize + "! = " + factorialResult);
+                    continue;
+                }
+                  //Calcul de l'arrangement via une fonction CalculR2 si saisie = 2
+                    
+                else if (c == 2)
+                {
+                        Console.Write("nombre total d'éléments à gérer = ");
+                        int t = int.Parse(Console.ReadLine());
+                        Console.Write("nombre d'éléments dans le sous ensemble = ");
+                        int subsetSize = int.Parse(Console.ReadLine());
+
+                        long factorialResult = CalculR2(subsetSize, t);
+                        Console.WriteLine("A(" + t + "/" + subsetSize + ") = " + factorialResult);
+                        continue;
+                }
+
+                    //Calcul de la combinaison via les deux fonctions si la saisie = 3
+
+                else if (c == 3)
+                {
                         Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
+                        int totalElements = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r
-                        long r = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r *= k;
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("A(" + t + "/" + n + ") = " + r);
-                    }
-                    else
-                    {
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
-                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        int subsetSize = int.Parse(Console.ReadLine()); // saisir le nombre
+
                         // calcul de r1
-                        long r1 = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r1 *= k;
+                        long factorialResult1 = CalculR2(subsetSize, totalElements);
                         // calcul de r2
-                        long r2 = 1;
-                        for (int k = 1; k <= n; k++)
-                            r2 *= k;
-                        // calcul de r3
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
-                    }
+                        long factorialResult2 = CalculR(subsetSize);
+                        Console.WriteLine("C(" + totalElements + "/" + subsetSize + ") = " + (factorialResult1 / factorialResult2));
+                        continue;
                 }
+                
             }
-            Console.ReadLine();
+            
         }
     }
 }
